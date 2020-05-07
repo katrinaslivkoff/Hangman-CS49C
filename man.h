7a07f7aa765e65f009367a4fdb1af4c75813e55a
Katrina Slivkoff <55104755+katrinaslivkoff@users.noreply.github.com>
2020-05-06 22:12:35 -0700

    Update man.h

--- man.h
+++ man.h
@@ -1,15 +1,19 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-#include <windows.h>
 
 void title(void); //function prototype for title
 void inst(void);       //function prototype for instructions
-void hangman(int, int);   //function prototype for hangman
+void hangman(int);   //function prototype for hangman
 //void credits(void);      //function prototype for credits
+
+void delay(unsigned int secs) {
+    unsigned int end = time(0) + secs;
+    while (time(0) < end);
+}
 
 void title(void)   //defining title
 {
+
     printf("--------------------------------------------\n");
     printf("| #  #   #   #   #  #### #   #   #   #   # |\n");
     printf("| #  #  # #  ##  # #     ## ##  # #  ##  # |\n");
@@ -17,10 +21,14 @@ void title(void)   //defining title
     printf("| #  # #   # #  ## #   # #   # #   # #  ## |\n");
     printf("| #  # #   # #   #  ###  #   # #   # #   # |\n");
     printf("--------------------------------------------\n\n");
+
+    delay(2);
+
+    printf("\033[0;0H\033[2J");
 }
 void inst(void)    //defining instructions
 {
-    system("CLS");
+
     printf("\n\n\n\t\t INSTRUCTIONS \n\t\t ============");
     printf("\n\n   1: HANGAMN IS THE GAME WHERE YOU HAVE TO GUESS THE WORD.");
     printf("\n   2: YOU WILL HAVE TO GUESS THE MAXIMUM WORDS FOR THE MAXIMUM SCORE.");
@@ -28,16 +36,19 @@ void inst(void)    //defining instructions
     printf("\n   4: 1 CORRECT LETTER = 1 SCORE");
     printf("\n   5: YOU HAVE THE n CHOICES TO FIND THE MAXIMUM WORDS\n\n\n");
 
-    system("PAUSE");          //time
-    system("CLS");               //clear the screen
+    delay(5);
+    printf("\033[0;0H\033[2J");
+
     printf("\n\n\n\n\t\t   HOW TO PLAY \n\t\t ===========");
     printf("\nUse your keyboard and guess the word . if you press any letter and the letter is in the word the game will tell you the position of the game . \nguess more , score more . \n\n \t\t\t BEST OF LUCK !");
-    system("PAUSE");
-    system("CLS");
+
+    delay(5);
+    printf("\033[0;0H\033[2J");
+
 }
-void hangman(int a, int score) {    //defining hangman
+void hangman(int a) {    //defining hangman
     switch (a) {
-    case 1:
+    case 6:
         {
             printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
             printf("  _______\n");
@@ -47,11 +58,44 @@ void hangman(int a, int score) {    //defining hangman
             printf("  |\n");
             printf("  |\n");
             printf("__|_________\n\n");
-            printf("\nYour score = %d", score);
-            printf("\n\t\t\t\t Chances Left=%d", 5 - a);
 
-            system("PAUSE");
         }
+        break;
+
+    case 5:
+        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
+        printf("  _______\n");
+        printf("  |/   | \n");
+        printf("  |    O \n");
+        printf("  |\n");
+        printf("  |\n");
+        printf("  |\n");
+        printf("__|_________\n\n");
+
+        break;
+
+    case 4:
+        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
+        printf("  _______\n");
+        printf("  |/   | \n");
+        printf("  |    O \n");
+        printf("  |    |\n");
+        printf("  |    |\n");
+        printf("  |\n");
+        printf("__|_________\n\n");
+
+        break;
+
+    case 3:
+        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
+        printf("  _______\n");
+        printf("  |/   | \n");
+        printf("  |    O \n");
+        printf("  |    |/\n");
+        printf("  |    |\n");
+        printf("  |\n");
+        printf("__|_________\n\n");
+
         break;
 
 
@@ -60,61 +104,36 @@ void hangman(int a, int score) {    //defining hangman
         printf("  _______\n");
         printf("  |/   | \n");
         printf("  |    O \n");
-        printf("  |\n");
-        printf("  |\n");
-        printf("  |\n");
-        printf("__|_________\n\n");
-        printf("\nYour score = %d", score);
-        printf("\n\t\t\t\t Chances Left=%d", 5 - a);
-        system("PAUSE");
-        break;
-
-    case 3:
-        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-        printf("  _______\n");
-        printf("  |/   | \n");
-        printf("  |    O \n");
-        printf("  |    |\n");
+        printf("  |   \\|/\n");
         printf("  |    |\n");
         printf("  |\n");
         printf("__|_________\n\n");
-        printf("\nYour score = %d", score);
-        printf("\n\t\t\t\t Chances Left=%d", 5 - a);
-        system("PAUSE");
+
         break;
 
+    case 1:
+    printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
+    printf("  _______\n");
+    printf("  |/   | \n");
+    printf("  |    O \n");
+    printf("  |   \\|/\n");
+    printf("  |    |\n");
+    printf("  |   /  \n");
+    printf("__|_________\n\n");
 
+    break;
 
+    case 0:
+    printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
+    printf("  _______\n");
+    printf("  |/   | \n");
+    printf("  |    x \n");
+    printf("  |   \\|/\n");
+    printf("  |    |\n");
+    printf("  |   / \\\n");
+    printf("__|_________\n\n");
 
-    case 4:
-        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-        printf("  _______\n");
-        printf("  |/   | \n");
-        printf("  |    O \n");
-        printf("  |   \\|/\n");
-        printf("  |    | \n");
-        printf("  |   /\n");
-        printf("__|_________\n\n");
-        printf("\nYour score = %d", score);
-        printf("\n\t\t\t\t Chances Left=%d", 5 - a);
-        system("PAUSE");
-        break;
-
-
-    case 5:
-        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-        printf("  _______\n");
-        printf("  |/   | \n");
-        printf("  |    X \n");
-        printf("  |   \\|/\n");
-        printf("  |    | \n");
-        printf("  |   / \\\n");
-        printf("__|_________\n\n");
-        printf("\nTotal score = %d", score);
-        printf("\n\t\t\t\t Chances Left=%d", 5 - a);
-        printf("\n\nYOU ARE DEAD ! GAME OVER!");
-        system("PAUSE");
-        break;
+    break;
 
     }
 }
