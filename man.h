2896f486cd97b763dbe1b8ac4aa74bd953838513
Katrina Slivkoff <55104755+katrinaslivkoff@users.noreply.github.com>
2020-05-18 18:04:55 -0700

    Update man.h

--- man.h
+++ man.h
@@ -14,7 +14,7 @@ void delay(unsigned int secs) {
 void title(void)   //defining title
 {
 
-    printf("--------------------------------------------\n");
+    printf("\n\n--------------------------------------------\n");
     printf("| #  #   #   #   #  #### #   #   #   #   # |\n");
     printf("| #  #  # #  ##  # #     ## ##  # #  ##  # |\n");
     printf("| #### ##### # # # #  ## # # # ##### # # # |\n");
@@ -25,24 +25,30 @@ void title(void)   //defining title
     delay(2);
 
     printf("\033[0;0H\033[2J");
+
+    printf("\n\nby Katrina Slivkoff and Ianna Duran \n");
+
+    delay(2);
+
+    printf("\033[0;0H\033[2J");
 }
+
 void inst(void)    //defining instructions
 {
 
-    printf("\n\n\n\t\t INSTRUCTIONS \n\t\t ============");
-    printf("\n\n   1: HANGAMN IS THE GAME WHERE YOU HAVE TO GUESS THE WORD.");
-    printf("\n   2: YOU WILL HAVE TO GUESS THE MAXIMUM WORDS FOR THE MAXIMUM SCORE.");
-    printf("\n   3: EVERY LEVEL WILL HAVE THE NEW WORD .");
-    printf("\n   4: 1 CORRECT LETTER = 1 SCORE");
-    printf("\n   5: YOU HAVE THE n CHOICES TO FIND THE MAXIMUM WORDS\n\n\n");
+    printf("\n\n         ------------\n\t\t INSTRUCTIONS \n\t\t ------------");
+    printf("\n\n   1: HANGAMN IS A GAME WHERE YOU HAVE TO GUESS THE CORRECT WORD");
+    printf("\n   2: EACH TURN YOU WILL GUESS A LETTER");
+    printf("\n   3: EVERY WRONG LETTER = -1 LIFE");
+    printf("\n   4: YOU HAVE 6 LIVES\n");
 
     delay(5);
     printf("\033[0;0H\033[2J");
 
-    printf("\n\n\n\n\t\t   HOW TO PLAY \n\t\t ===========");
-    printf("\nUse your keyboard and guess the word . if you press any letter and the letter is in the word the game will tell you the position of the game . \nguess more , score more . \n\n \t\t\t BEST OF LUCK !");
+    printf("\n\n\n\n\t\t HOW TO PLAY \n\t\t ===========");
+    printf("\n Use your keyboard to guess a letter. If correct, the letter will be placed           \n where it is supposed to be in the word. If incorrect, no letters are                 \n placed and the hangamn starts to get drawn.                                          \n GOOD LUCK!!! \n");
 
-    delay(5);
+    delay(6);
     printf("\033[0;0H\033[2J");
 
 }
@@ -50,9 +56,8 @@ void hangman(int a) {    //defining hangman
     switch (a) {
     case 6:
         {
-            printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-            printf("  _______\n");
-            printf("  |\n");
+            printf("\n  _______\n");
+            printf("  |/\n");
             printf("  |\n");
             printf("  |\n");
             printf("  |\n");
@@ -63,8 +68,7 @@ void hangman(int a) {    //defining hangman
         break;
 
     case 5:
-        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-        printf("  _______\n");
+        printf("\n  _______\n");
         printf("  |/   | \n");
         printf("  |    O \n");
         printf("  |\n");
@@ -75,8 +79,7 @@ void hangman(int a) {    //defining hangman
         break;
 
     case 4:
-        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-        printf("  _______\n");
+        printf("\n  _______\n");
         printf("  |/   | \n");
         printf("  |    O \n");
         printf("  |    |\n");
@@ -87,8 +90,7 @@ void hangman(int a) {    //defining hangman
         break;
 
     case 3:
-        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-        printf("  _______\n");
+        printf("\n  _______\n");
         printf("  |/   | \n");
         printf("  |    O \n");
         printf("  |    |/\n");
@@ -100,8 +102,7 @@ void hangman(int a) {    //defining hangman
 
 
     case 2:
-        printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-        printf("  _______\n");
+        printf("\n  _______\n");
         printf("  |/   | \n");
         printf("  |    O \n");
         printf("  |   \\|/\n");
@@ -112,26 +113,24 @@ void hangman(int a) {    //defining hangman
         break;
 
     case 1:
-    printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-    printf("  _______\n");
-    printf("  |/   | \n");
-    printf("  |    O \n");
-    printf("  |   \\|/\n");
-    printf("  |    |\n");
-    printf("  |   /  \n");
-    printf("__|_________\n\n");
+      printf("\n  _______\n");
+      printf("  |/   | \n");
+      printf("  |    O \n");
+      printf("  |   \\|/\n");
+      printf("  |    |\n");
+      printf("  |   /  \n");
+      printf("__|_________\n\n");
 
     break;
 
     case 0:
-    printf("\n\t\t\tAmount of wrong letters: %d\n\n", a);
-    printf("  _______\n");
-    printf("  |/   | \n");
-    printf("  |    x \n");
-    printf("  |   \\|/\n");
-    printf("  |    |\n");
-    printf("  |   / \\\n");
-    printf("__|_________\n\n");
+      printf("\n  _______\n");
+      printf("  |/   | \n");
+      printf("  |    x \n");
+      printf("  |   \\|/\n");
+      printf("  |    |\n");
+      printf("  |   / \\\n");
+      printf("__|_________\n\n");
 
     break;
 
