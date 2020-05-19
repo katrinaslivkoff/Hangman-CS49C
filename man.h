ac190f771150abdfc80b37815b49e2033f52e877
Katrina Slivkoff <55104755+katrinaslivkoff@users.noreply.github.com>
2020-05-18 19:21:23 -0700

    Update man.h

--- man.h
+++ man.h
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void title(void); //function prototype for title
-void inst(void);       //function prototype for instructions
-void hangman(int);   //function prototype for hangman
+void title(void); 
+void inst(void);       
+void hangman(int);   
 
+//defines the delay method to delay the screen
 void delay(unsigned int secs) {
     unsigned int end = time(0) + secs;
     while (time(0) < end);
 }
 
-void title(void)   //defining title
+//defines the title and authors
+void title(void)  
 {
 
     printf("\n\n--------------------------------------------\n");
@@ -21,18 +23,19 @@ void title(void)   //defining title
     printf("| #  # #   # #   #  ###  #   # #   # #   # |\n");
     printf("--------------------------------------------\n\n");
 
-    delay(2);
+    delay(2); // delay screen for 2 seconds
 
-    printf("\033[0;0H\033[2J");
+    printf("\033[0;0H\033[2J"); // clears the screen
 
     printf("\n\nby Katrina Slivkoff and Ianna Duran \n");
 
-    delay(2);
+    delay(2); // delays the screen for 2 seconds
 
-    printf("\033[0;0H\033[2J");
+    printf("\033[0;0H\033[2J"); // clears the screen
 }
 
-void inst(void)    //defining instructions
+//defines the instruction of the game
+void inst(void)   
 {
 
     printf("\n\n         ------------\n\t\t INSTRUCTIONS \n\t\t ------------");
@@ -41,17 +44,22 @@ void inst(void)    //defining instructions
     printf("\n   3: EVERY WRONG LETTER = -1 LIFE");
     printf("\n   4: YOU HAVE 6 LIVES\n");
 
-    delay(5);
-    printf("\033[0;0H\033[2J");
+    delay(5); // delay screen for 5 seconds
+
+    printf("\033[0;0H\033[2J"); // clears the screen
 
     printf("\n\n\n\n\t\t HOW TO PLAY \n\t\t ===========");
     printf("\n Use your keyboard to guess a letter. If correct, the letter will be placed           \n where it is supposed to be in the word. If incorrect, no letters are                 \n placed and the hangamn starts to get drawn.                                          \n GOOD LUCK!!! \n");
 
-    delay(6);
-    printf("\033[0;0H\033[2J");
+    delay(6); // delay screen for 6 seconds
+
+    printf("\033[0;0H\033[2J"); // clears the screen
 
 }
-void hangman(int a) {    //defining hangman
+
+//defines the hangman drawing for each case
+void hangman(int a) 
+{    
     switch (a) {
     case 6:
         printf("\n  _______\n");
